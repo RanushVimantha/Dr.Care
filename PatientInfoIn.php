217c436018707c6778e5e36fd5f65722864d8071
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2023-10-29 11:37:47 +0530

    Database ,process.php and temporary Penitent view table
    Made a Penitent info input form.
    Created the whole database(Modifications can be done if needed)
    Made a temporary Penitent view table.

---
+++ PatientInfoIn.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Patient Info</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container my-5">
+    <header class="d-flex justify-content-between my-4">
+            <div>
+            <a href="index.php" class="btn btn-primary">Back</a>
+            </div>
+        </header>
+
+  <div class="wholeform">
+    <form id="PatientInfo" name="PatientInfo" action="process.php" method="post" class="row g-3 needs-validation" novalidate>
+      <h1>Patient Info</h1>
+
+        <!-- First Name -->
+
+      <div class="form-floating mb-3">
+        <input type="text" class="form-control" name="firstname" placeholder="Enter Your First Name" id="validationCustom01" required>
+        <label for="floatingInput">First Name</label>
+        <div class="invalid-feedback"> Should Enter the First Name! </div>
+      </div>
+
+      <!-- Second Name -->
+
+
+      <div class="form-floating mb-3">
+        <input type="text" class="form-control" name="lastname" placeholder="Enter Your Last Name" id="validationCustom02" required>
+        <label for="floatingInput">Last Name</label>
+        <div class="invalid-feedback"> Should Enter the Last Name! </div>
+      </div>
+
+      <!-- Email -->
+
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" name="email" placeholder="Enter Your Email" id="validationCustom03" required>
+        <label for="floatingInput">Email address</label>
+        <div class="invalid-feedback"> Should be a Valid Email! </div>
+      </div>
+
+      <!-- Gender -->
+
+      <div class="form-select">
+        <select name="gender" class="form-control" id="validationCustom05" required>
+          <option selected disabled value="">Select Gender:</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Other">Other</option>
+        </select>
+        <div class="invalid-feedback"> Please select a valid Gender. </div>
+      </div>
+
+      <!-- Phone Number -->
+
+      <span class="number">
+
+        <label for="floatingInput">Phone Number : </label>
+        <input type="tel" id="validationCustom04" name="contactnumber" placeholder="094-778-91-5586" pattern="[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{4}" required>
+        <div class="invalid-feedback"> Enter the Phone Number in this format ( 094-778-91-5586 )</div>
+      </span>
+
+      <!-- DOB -->
+
+      <span class="DOB">
+        <label for="floatingInput">Date Of Birth : </label>
+        <input type="date" name="date" id="validationCustom04" required>
+        <div class="invalid-feedback"> Birth Date is Required! </div>
+      </span>
+
+      <!-- Diagnosis -->
+
+      <div class="form-floating">
+        <textarea class="form-control" name="Diagnosis" placeholder="Give the Diagnosis here" style="height: 200px"></textarea>
+        <label for="floatingTextarea2">Diagnosis</label>
+      </div>
+
+      <!-- Medications -->
+
+      <div class="form-floating">
+        <textarea class="form-control" name="Medications" placeholder="Give the Medicationsis here" style="height: 200px"></textarea>
+        <label for="floatingTextarea2">Medications</label>
+      </div>
+
+      <!-- Submit -->
+
+      <div class="form-element my-4">
+                <input type="submit" name="create" value="Add Patient" class="btn btn-primary">
+            </div>
+
+    </form>
+  </div>
+</div>
+  <script>
+    (() => {
+      'use strict'
+
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      const forms = document.querySelectorAll('.needs-validation')
+
+      // Loop over them and prevent submission
+      Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+    })()
+  </script>
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,95 @@
+<?php
+include('connect.php');
+if (isset($_POST["create"])) {
+    $FirstName = mysqli_real_escape_string($conn, $_POST["firstname"]);
+    $LastName = mysqli_real_escape_string($conn, $_POST["lastname"]);
+    $DateOfBirth = date('Y-m-d', strtotime($_POST['date']));
+    $Gender = mysqli_real_escape_string($conn, $_POST["gender"]);
+    $ContactNumber = mysqli_real_escape_string($conn, $_POST["contactnumber"]);
+    $Email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $Diagnosis = mysqli_real_escape_string($conn, $_POST["Diagnosis"]);
+    $Medications = mysqli_real_escape_string($conn, $_POST["Medications"]);
+
+    // Use prepared statements
+    $stmt = $conn->prepare("INSERT INTO patients (FirstName, LastName, DateOfBirth, Gender, ContactNumber, Email) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssss", $FirstName, $LastName, $DateOfBirth, $Gender, $ContactNumber, $Email);
+    
+    // Start a transaction
+    $conn->begin_transaction();
+
+    if ($stmt->execute()) {
+        // Insert patient succeeded, now insert medical records
+        $stmt = $conn->prepare("INSERT INTO medicalrecords (PatientID, Diagnosis, Medications) VALUES (?, ?, ?)");
+        $stmt->bind_param("iss", $conn->insert_id, $Diagnosis, $Medications);
+
+        if ($stmt->execute()) {
+            // Both inserts successful, commit the transaction
+            $conn->commit();
+            session_start();
+            $_SESSION["create"] = "Patient Recorded Successfully!";
+            header("Location:index.php");
+        } else {
+            // Medical records insertion failed, roll back the transaction
+            $conn->rollback();
+            die("Something went wrong with medical records insertion.");
+        }
+    } else {
+        // Patient insertion failed
+        die("Something went wrong with patient insertion.");
+    }
+}
+
+
+// OLD CODE
+
+/* include('connect.php');
+if (isset($_POST["create"])) {
+    $FirstName = mysqli_real_escape_string($conn, $_POST["firstname"]);
+    $LastName = mysqli_real_escape_string($conn, $_POST["lastname"]);
+    $DateOfBirth = date('Y-m-d', strtotime($_POST['date']));
+    $Gender = mysqli_real_escape_string($conn, $_POST["gender"]);
+    $ContactNumber = mysqli_real_escape_string($conn, $_POST["contactnumber"]);
+    $Email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $Diagnosis = mysqli_real_escape_string($conn, $_POST["Diagnosis"]);
+    $Medications = mysqli_real_escape_string($conn, $_POST["Medications"]);
+
+    $sqlInsert = "INSERT INTO patients(FirstName , LastName , DateOfBirth , Gender , ContactNumber , Email) VALUES ('$FirstName','$LastName','$DateOfBirth','$Gender','$ContactNumber','$Email')";
+    if(mysqli_query($conn,$sqlInsert)){
+        session_start();
+        $_SESSION["create"] = "Patitent Recorded Sucessfully!";
+        header("Location:index.php");
+    }else{
+        die("Something went wrong");
+    }
+
+    $sqlInsert = "INSERT INTO medicalrecords(Diagnosis , Medications) VALUES ('$Diagnosis','$Medications')";
+    if(mysqli_query($conn,$sqlInsert)){
+        session_start();
+        $_SESSION["create"] = "Patitent Recorded Sucessfully!";
+        header("Location:index.php");
+    }else{
+        die("Something went wrong");
+    }
+
+} */
+
+
+// CODE FOR EDITING (HAVE TO MAKE)
+
+
+/* if (isset($_POST["edit"])) {
+    $title = mysqli_real_escape_string($conn, $_POST["title"]);
+    $type = mysqli_real_escape_string($conn, $_POST["type"]);
+    $author = mysqli_real_escape_string($conn, $_POST["author"]);
+    $description = mysqli_real_escape_string($conn, $_POST["description"]);
+    $id = mysqli_real_escape_string($conn, $_POST["id"]);
+    $sqlUpdate = "UPDATE books SET title = '$title', type = '$type', author = '$author', description = '$description' WHERE id='$id'";
+    if(mysqli_query($conn,$sqlUpdate)){
+        session_start();
+        $_SESSION["update"] = "Book Updated Successfully!";
+        header("Location:index.php");
+    }else{
+        die("Something went wrong");
+    }
+} */
+?>
