9eeb74ab5de1d95e7253faa0f66adc140414c7f5
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2024-03-03 13:18:04 +0530

    new update

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+// Start a session
+session_start();
+
+// Check if the user is not logged in, redirect to login.php
+if (!isset($_SESSION['DoctorID'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +17,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<<<<<<< Updated upstream
     <title>Book List</title>
+=======
+    <title>Patient List</title>
+    <link rel="icon" href="imgs/drcare.ico" type="image/x-icon">
+>>>>>>> Stashed changes
     <style>
         table td,
         table th {
@@ -19,100 +35,121 @@
 
 <body>
     <div class="container my-4">
-        <header class="d-flex justify-content-between my-4">
-            <h1>Patient List</h1>
+        <?php
+        // Linking Database.php
+        require "db/DataBase.php";
+        $database = new DataBase();
+        $conn = $database->dbConnect();
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        // Get the DoctorID from $_SESSION['DoctorID']
+        $doctorID = $_SESSION['DoctorID'];
+        // Showing All the Diagnosis and Medications relavant to the Patient
+
+        $sql = "SELECT * FROM Doctors WHERE DoctorID = $doctorID";
+        $result = mysqli_query($conn, $sql);
+        while ($data = mysqli_fetch_array($result)) {
+        ?>
+            <header class="d-flex justify-content-between my-4">
+
+                <h1>Welcome <?php echo $data['FirstName'];?> <?php echo $data['LastName'];?></h1>
+
+            <?php
+        }
+            ?>
+            <h2>Patient List</h2>
             <div>
                 <a href="patientInfoIn.php" class="btn btn-primary">Add New Patient</a>
             </div>
-        </header>
-        <?php
-        session_start();
-        if (isset($_SESSION["create"])) {
-        ?>
-            <div class="alert alert-success">
-                <?php
-                echo $_SESSION["create"];
-                ?>
-            </div>
-        <?php
-            unset($_SESSION["create"]);
-        }
-        ?>
-        <?php
-        if (isset($_SESSION["update"])) {
-        ?>
-            <div class="alert alert-success">
-                <?php
-                echo $_SESSION["update"];
-                ?>
-            </div>
-        <?php
-            unset($_SESSION["update"]);
-        }
-        ?>
-        <?php
-        if (isset($_SESSION["delete"])) {
-        ?>
-            <div class="alert alert-success">
-                <?php
-                echo $_SESSION["delete"];
-                ?>
-            </div>
-        <?php
-            unset($_SESSION["delete"]);
-        }
-        ?>
+            <div>
+                <form method="post" action="">
+                    <input type="submit" class="btn btn-primary" name="logout" value="Logout">
 
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>PatientID</th>
-                    <th>FirstName</th>
-                    <th>LastName</th>
-                    <th>Gender</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
+                    <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['logout'])) {
+                        $database = new DataBase();
+                        $database->Logoutbutton();
+                    }
+                    ?>
+                </form>
+            </div>
+            </header>
+            <?php
+            if (isset($_SESSION["create"])) {
+            ?>
+                <div class="alert alert-success">
+                    <?php
+                    echo $_SESSION["create"];
+                    ?>
+                </div>
+            <?php
+                unset($_SESSION["create"]);
+            }
+            ?>
+            <?php
+            if (isset($_SESSION["update"])) {
+            ?>
+                <div class="alert alert-success">
+                    <?php
+                    echo $_SESSION["update"];
+                    ?>
+                </div>
+            <?php
+                unset($_SESSION["update"]);
+            }
+            ?>
+            <?php
+            if (isset($_SESSION["delete"])) {
+            ?>
+                <div class="alert alert-success">
+                    <?php
+                    echo $_SESSION["delete"];
+                    ?>
+                </div>
+            <?php
+                unset($_SESSION["delete"]);
+            }
+            ?>
 
-                <?php
-                include('connect.php');
-                $sqlSelect = "SELECT * FROM patients";
-                $result = mysqli_query($conn, $sqlSelect);
-                while ($data = mysqli_fetch_array($result)) {
-                ?>
+            <table class="table table-bordered">
+                <thead>
                     <tr>
-                        <td><?php echo $data['PatientID']; ?></td>
-                        <td><?php echo $data['FirstName']; ?></td>
-                        <td><?php echo $data['LastName']; ?></td>
-                        <td><?php echo $data['Gender']; ?></td>
-                        <td>
-                            <a href="PatientView.php?id=<?php echo $data['PatientID']; ?>" class="btn btn-info">Read More</a>
-                            <a href="PatientInfoEdit.php?id=<?php echo $data['PatientID']; ?>" class="btn btn-warning">Edit</a>
-                            <a href="PatientDelete.php?id=<?php echo $data['PatientID']; ?>" class="btn btn-danger" id="delete">Delete Medical Record</a>
-                        </td>
+                        <th>PatientID</th>
+                        <th>FirstName</th>
+                        <th>LastName</th>
+                        <th>Gender</th>
+                        <th>Action</th>
                     </tr>
-                <?php
-                }
-                ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+
+                    <?php
+
+
+
+                    $sqlSelect = "SELECT * FROM patients";
+                    $result = mysqli_query($conn, $sqlSelect);
+                    while ($data = mysqli_fetch_array($result)) {
+                    ?>
+                        <tr>
+                            <td><?php echo $data['PatientID']; ?></td>
+                            <td><?php echo $data['FirstName']; ?></td>
+                            <td><?php echo $data['LastName']; ?></td>
+                            <td><?php echo $data['Gender']; ?></td>
+                            <td>
+                                <a href="PatientView.php?id=<?php echo $data['PatientID']; ?>" class="btn btn-info">Read More</a>
+                                <a href="PatientInfoEdit.php?id=<?php echo $data['PatientID']; ?>" class="btn btn-warning">Edit</a>
+                                <a href="PatientDelete.php?id=<?php echo $data['PatientID']; ?>" class="btn btn-danger">Delete Patient</a>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
     </div>
-
-
-    <script>
-        var link = document.getElementById("delete");
-        var initialName = "Delete Patient";
-        var changedName = "Delete Medical Records";
-
-        if (localStorage.getItem("linkName") === changedName) {
-            link.innerText = initialName;
-            localStorage.setItem("linkName", initialName);
-        } else {
-            link.innerText = changedName;
-            localStorage.setItem("linkName", changedName);
-        }
-    </script>
 
 </body>
 
---
+++ signup.php
@@ -0,0 +1,128 @@
+<?php
+require "db/DataBase.php";
+$db = new DataBase();
+if ($db->dbConnect()) {
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dr.Care | Signup</title>
+        <link rel="icon" href="imgs/drcare.ico" type="image/x-icon">
+        <link rel="stylesheet" href="styles/login_signup.css">
+    </head>
+
+    <body>
+        <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+            <span></span>
+
+            <div class="signin">
+
+                <div class="content">
+
+                    <h2>Sign Up</h2>
+
+                    <div class="form">
+
+                        <form id="login-form" name="Login" action="" method="POST">
+
+                            <div class="inputBox">
+
+                                <input type="text" name="FirstName" required> <i>First Name</i>
+
+                            </div>
+
+                            <div class="inputBox">
+
+                                <input type="text" name="LastName" required> <i>Last Name</i>
+
+                            </div>
+
+                            <div class="inputBox">
+
+                                <input type="email" name="Email" required> <i>Email</i>
+
+                            </div>
+
+                            <div class="inputBox">
+
+                                <input type="text" name="Specialization" required> <i>Specialization</i>
+
+                            </div>
+
+                            <div class="inputBox">
+
+                                <input class="passwordinput" type="password" name="Password" required> <i>Password</i>
+
+                            </div>
+
+                            <div class="links"> <a href="#">Forgot Password</a> <a href="login.php">Login</a>
+
+                            </div>
+
+                            <div class="inputBox">
+
+                                <input class="registerbutton" type="submit" value="Register">
+
+                                <div class="loginerror">
+                                <?php
+                                if (isset($_POST['FirstName']) && isset($_POST['LastName']) && isset($_POST['Email']) && isset($_POST['Specialization']) && isset($_POST['Password'])) {
+                                    if ($db->signUp("doctors", $_POST['Email'], $_POST['Password'], $_POST['FirstName'], $_POST['LastName'], $_POST['Specialization'])) {
+                                        echo "Sign Up Success";
+                                    } else echo "<br> Sign up Failed";
+                                } else echo "All fields are required";
+                            } else echo "Error: Database connection";
+                                ?>
+                                </div>
+                        </form>
+                    </div>
+
+                </div>
+
+            </div>
+
+            </div>
+
+        </section>
+    </body>
+
+    </html>
