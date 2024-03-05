a645f5b082f53606a542a3c1c809c0f6371efd47
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2024-03-05 22:24:24 +0530

    Replacing the old login with the new login

--- login.php
+++ login.php
@@ -7,126 +7,77 @@ session_start();
 
 // Check if the user is already logged in
 if (isset($_SESSION['DoctorID'])) {
-    // If logged in, redirect to a dashboard or home page
-    header("Location: index.php");
-    exit();
+  // If logged in, redirect to a dashboard or home page
+  header("Location: index.php");
+  exit();
 }
 
 $db = new DataBase();
 if ($db->dbConnect()) {
 ?>
+  <!DOCTYPE html>
+  <html lang="en">
 
-    <!DOCTYPE html>
-    <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dr-care</title>
+    <link rel="stylesheet" href="styles/new_style.css">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Dr.Care Login</title>
-        <link rel="icon" href="imgs/drcare.ico" type="image/x-icon">
-        <link rel="stylesheet" href="styles/login_signup.css">
-    </head>
+  </head>
 
-    <body>
-        <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-            <span></span>
+  <body>
+    <?php include('header.html'); ?>
+    <script type="text/javascript" src="js/light-dark.js"></script>
 
-            <div class="signin">
 
-                <div class="content">
+    <div class="login-page">
+      <div class="form">
+        <img src="imgs/logo.png" style="width: 80%; height: auto; margin-bottom: 1.2rem;">
+        <!-- <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form> -->
+        <form class="login-form" id="login-form" name="Login" action="" method="POST">
+          <input type="Email" name="Email" placeholder="Email" />
+          <input type="password" name="Password" placeholder="Password" />
+          <input type="submit" class="login-submit" value="login">
 
-                    <h2>Login</h2>
+          <div class="errortext">
 
-                    <div class="form">
+          <?php
+          if (isset($_POST['Email']) && isset($_POST['Password'])) {
+            if ($db->logIn("doctors", $_POST['Email'], $_POST['Password'])) {
+              $_SESSION['Email'] = $db->prepareData($_POST['Email']);
+              // Set a temporary session timeout
+              $_SESSION['timeout'] = time() + 3600;
+              header("Location: index.php");
+              exit();
+            } else {
+              // echo "Username or Password wrong";
+            }
+          } else echo "All fields are required";
+        } else echo "Database Connection Error";
+        ob_end_flush(); // Send the buffered output to the browser and turn off output buffering
+          ?>
+          </div>
+          <p class="message">Not registered? <a href="#">Create an account</a></p>
+        </form>
+      </div>
+    </div>
+    <script>
+      $('.message a').click(function() {
+        $('form').animate({
+          height: "toggle",
+          opacity: "toggle"
+        }, "slow");
+      });
+    </script>
 
-                        <div class="inputBox">
+    <?php include('footer.html'); ?>
+  </body>
 
-                            <form id="login-form" name="Login" action="" method="POST">
-
-                                <input type="text" name="Email" required> <i>Email</i>
-
-                        </div>
-
-                        <div class="inputBox">
-
-                            <input type="password" name="Password" required> <i>Password</i>
-
-                        </div>
-
-                        <div class="links"> <a href="#">Forgot Password</a> <a href="signup.php">Signup</a>
-
-                        </div>
-
-                        <div class="inputBox">
-
-                            <input type="submit" value="Login">
-
-                            <div class="loginerror">
-
-                            <?php
-                            if (isset($_POST['Email']) && isset($_POST['Password'])) {
-                                if ($db->logIn("doctors", $_POST['Email'], $_POST['Password'])) {
-                                    $_SESSION['Email'] = $db->prepareData($_POST['Email']);
-                                    // Set a temporary session timeout
-                                    $_SESSION['timeout'] = time() + 3600;
-                                    header("Location: index.php");
-                                    exit();
-                                } else {
-                                    // echo "Username or Password wrong";
-                                }
-                            } else echo "All fields are required";
-                        } else echo "Database Connection Error";
-                        ob_end_flush(); // Send the buffered output to the browser and turn off output buffering
-                            ?>
-                            </div>
-
-                            </form>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </section>
-    </body>
-
-    </html>
+  </html>
