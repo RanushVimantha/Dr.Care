e7ce723aacd49a4a23240cd759ccb9f8cfe6d776
BGHiruniEK <hirueranga5@gmail.com>
2024-03-08 17:02:30 +0530

    profile
    further edits

--- profiles.php
+++ profiles.php
@@ -75,7 +75,7 @@
 
             <div class="contentBx">
 
-                <h2>Dr. Randil Jay</h2>
+                <h2>Dr. Pramod Gannearachchi</h2>
 
                 <div class="spec">
                     <h3>Specialist In :</h3>
@@ -86,7 +86,7 @@
                 <div class="spec">
 
                     <h3>Hospital :</h3>
-                    <span>Ragama District</span>
+                    <span>Colombo District</span>
 
                 </div>
                 <a href="#">View Patitents</a>
@@ -95,5 +95,146 @@
         </div>
     </div>
 </div>
+<div class="drCardcontainer">
+        <div class="card">
+            <div class="imgBx">
+                <img src="imgs/Logo.png" alt="Doctor-Image">
+            </div>
+
+            <div class="contentBx">
+
+                <h2>Dr. Prasad Wijesuriya</h2>
+
+                <div class="spec">
+                    <h3>Specialist In :</h3>
+                    <span>Specialisted Area</span>
+
+                </div>
+
+                <div class="spec">
+
+                    <h3>Hospital :</h3>
+                    <span>Colombo District</span>
+
+                </div>
+                <a href="#">View Patitents</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="drCardcontainer">
+        <div class="card">
+            <div class="imgBx">
+                <img src="imgs/Logo.png" alt="Doctor-Image">
+            </div>
+
+            <div class="contentBx">
+
+                <h2>Dr. Mahela Thilakarathne</h2>
+
+                <div class="spec">
+                    <h3>Specialist In :</h3>
+                    <span>Specialisted Area</span>
+
+                </div>
+
+                <div class="spec">
+
+                    <h3>Hospital :</h3>
+                    <span>Colombo District</span>
+
+                </div>
+                <a href="#">View Patitents</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="drCardcontainer">
+        <div class="card">
+            <div class="imgBx">
+                <img src="imgs/Logo.png" alt="Doctor-Image">
+            </div>
+
+            <div class="contentBx">
+
+                <h2>Dr. Nimali Gunarathne</h2>
+
+                <div class="spec">
+                    <h3>Specialist In :</h3>
+                    <span>Specialisted Area</span>
+
+                </div>
+
+                <div class="spec">
+
+                    <h3>Hospital :</h3>
+                    <span>Colombo District</span>
+
+                </div>
+                <a href="#">View Patitents</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="drCardcontainer">
+        <div class="card">
+            <div class="imgBx">
+                <img src="imgs/Logo.png" alt="Doctor-Image">
+            </div>
+
+            <div class="contentBx">
+
+                <h2>Dr. Sachin Liyanage</h2>
+
+                <div class="spec">
+                    <h3>Specialist In :</h3>
+                    <span>Specialisted Area</span>
+
+                </div>
+
+                <div class="spec">
+
+                    <h3>Hospital :</h3>
+                    <span>Colombo District</span>
+
+                </div>
+                <a href="#">View Patitents</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="drCardcontainer">
+        <div class="card">
+            <div class="imgBx">
+                <img src="imgs/Logo.png" alt="Doctor-Image">
+            </div>
+
+            <div class="contentBx">
+
+                <h2>Dr. Isuru Gamage</h2>
+
+                <div class="spec">
+                    <h3>Specialist In :</h3>
+                    <span>Specialisted Area</span>
+
+                </div>
+
+                <div class="spec">
+
+                    <h3>Hospital :</h3>
+                    <span>Colombo District</span>
+
+                </div>
+                <a href="#">View Patitents</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+<?php include('footer.html'); ?>
 </body>
 </html>
