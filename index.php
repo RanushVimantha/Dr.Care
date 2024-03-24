cc0dbcfb20e0ad2d9aa64ff0308fb5d9e29b774e
RanushVimantha <ranushvimantha1@gmail.com>
2024-03-24 17:11:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['DoctorID'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/dashboard.css">
-    <title>Patient List</title>
+    <title>Dashboard - DrCare</title>
     <link rel="icon" href="imgs/drcare.ico" type="image/x-icon">
 </head>
 
