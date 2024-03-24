8e496f8c516e16ae92e6faa06b846727132eb024
RanushVimantha <ranushvimantha1@gmail.com>
2024-03-24 17:16:55 +0530

    Update login.php

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if ($db->dbConnect()) {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dr-care</title>
+    <title>Login - DrCare</title>
     <link rel="stylesheet" href="styles/new_style.css">
 
   </head>
